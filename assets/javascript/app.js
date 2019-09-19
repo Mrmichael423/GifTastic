b09a0e6b35407ff99cffca63c0210be390855d68
Kevin Halstead <mrmichael423@icloud.com>
2019-09-18 18:49:04 -0600

    added buttons to the page from the javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,25 @@
+var topics = ["Kevin Hart", "Dave Chappelle", "Tim Allen", "Eddie Murphy", "George Carlin", "Robin Williams", "Rodney Dangerfield", "Chris Rock", "Will Ferrell", "Bill Cosby"]
+
+function renderButtons() {
+    $("#buttons").empty();
+
+    for(var i = 0; i< topics.length; i++){
+        
+        var gif = $("<button>");
+        gif.addClass("comic");
+        gif.attr("data-name", topics[i]);
+        gif.text(topics[i]);
+        $("#buttons").append(gif);
+    }
+}
+renderButtons()
+console.log(topics)
+console.log(renderButtons)
+
+
+
+
+
+
 var queryUrl = "https://api.giphy.com/v1/gifs/" + search + "?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit:10";
 var search = "";
-var topics = ["Kevin Hart", "Dave Chappelle", "Tim Allen", "Eddie Murphy", "George Carlin", "Robin Williams", "Rodney Dangerfield", "Chris Rock", "Will Ferrell", "Bill Cosby"]
