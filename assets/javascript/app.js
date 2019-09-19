cd2ed042da00c9f7c96ccf6c8d24916bcfa2c095
Kevin Halstead <mrmichael423@icloud.com>
2019-09-18 18:10:04 -0600

    added css and javascript folder and started on var in javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,3 @@
+var queryUrl = "https://api.giphy.com/v1/gifs/" + search + "?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit:10";
+var search = "";
+var topics = ["Kevin Hart", "Dave Chappelle", "Tim Allen", "Eddie Murphy", "George Carlin", "Robin Williams", "Rodney Dangerfield", "Chris Rock", "Will Ferrell", "Bill Cosby"]
