0cd257af721cba3bc13fdde37b5d6ced88595702
Kevin Halstead <mrmichael423@icloud.com>
2019-09-18 19:27:48 -0600

    able to conncet with the api to pull data and added a new div for the gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,12 +20,21 @@ $("#add-gif").on("click", function(event){
     topics.push(newGif);
     renderButtons();
 })
+$("#buttons").on("click", ".comic", function() {
+    var search = $(this).attr("data-person");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    search + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        var results = response.data
+
+    })
+
+})
+
+
 renderButtons()
-
-
-
-
-
-
-var queryUrl = "https://api.giphy.com/v1/gifs/" + search + "?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit:10";
-var search = "";
